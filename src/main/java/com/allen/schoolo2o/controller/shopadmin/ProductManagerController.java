281fe62848849220f2719dc5e7bcb44a737585f1
allen <1136034405@qq.com>
2018-06-10 21:45:31 +0800

    商品类别删除

--- src/main/java/com/allen/schoolo2o/controller/shopadmin/ProductManagerController.java
+++ src/main/java/com/allen/schoolo2o/controller/shopadmin/ProductManagerController.java
@@ -63,24 +63,58 @@ public class ProductManagerController {
 		}
 		if (productCategoryList != null && productCategoryList.size() > 0) {
 			try {
-			ProductCategoryExecution productCategoryExecution =productCategoryService.batchAddProductCategory(productCategoryList);
-			if(productCategoryExecution.getState() == ProductCategoryEnum.SUCCESS.getCode()) {
-				modelMap.put("success", true);
-			}else {
-				modelMap.put("success", false);
-				modelMap.put("errMsg", productCategoryExecution.getStateInfo());
-			}}catch (Exception e) {
+				ProductCategoryExecution productCategoryExecution = productCategoryService
+						.batchAddProductCategory(productCategoryList);
+				if (productCategoryExecution.getState() == ProductCategoryEnum.SUCCESS.getCode()) {
+					modelMap.put("success", true);
+				} else {
+					modelMap.put("success", false);
+					modelMap.put("errMsg", productCategoryExecution.getStateInfo());
+				}
+			} catch (Exception e) {
 				modelMap.put("success", false);
 				modelMap.put("errMsg", e.getMessage());
 				return modelMap;
 			}
-			
 
-		}else {
+		} else {
 			modelMap.put("success", false);
-			modelMap.put("errMsg","请至少输入一个商品类别");
+			modelMap.put("errMsg", "请至少输入一个商品类别");
 		}
 
 		return modelMap;
 	}
+
+	@RequestMapping(value = "/removeproductcategorys", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> removeproductcategory(Long productCategoryId, HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+
+		if (productCategoryId != null && productCategoryId > 0) {
+			try {
+				Shop currentShop = (Shop) request.getSession().getAttribute("currentShop");
+				ProductCategoryExecution productCategoryExecution = productCategoryService
+						.deleteProductCategory(productCategoryId, currentShop.getShopId());
+				if (productCategoryExecution.getState() == ProductCategoryEnum.SUCCESS.getCode()) {
+					modelMap.put("success", true);
+
+				} else {
+					modelMap.put("success", false);
+					modelMap.put("errMsg", productCategoryExecution.getStateInfo());
+				}
+			} catch (Exception e) {
+				modelMap.put("success", false);
+				modelMap.put("errMsg", e.getMessage());
+				return modelMap;
+			}
+
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "至少选择一个商品类别");
+
+		}
+		return modelMap;
+
+	}
+
 }
